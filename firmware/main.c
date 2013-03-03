2f5853abce829a48fe12cc8d7606c2122e100522
Andreas Weber <andy.weber.aw@gmail.com>
2013-03-03 22:23:29 +0100

    AVR firmware erweitert

--- firmware/main.c
+++ firmware/main.c
@@ -12,17 +12,31 @@
 	/SD obere Halbbrücke PD6
 	IN untere Halbbrücke PD5, OC1A
 	/SD untere Halbbrücke PD7
+   
+  Relais für Heizung: PB3
+  PT100: TBD: ggf. mit Messwandler
+   
+  LCD:
+  S99.5°C  I77.3°C
+  RH....
+   
+  Buchstaben in der unteren Zeile sind die Bits in s_status.enable
+  Bit 0, R: Temperaturregelung aktiv
+  Bit 1, H: Heizung eingeschaltet
+  noch weiter definieren: Steuerung usw.
 
 **************************************************/
 
 #define F_CPU 16000000UL
 
 #include <stdlib.h>
+#include <string.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
 #include "lcd.h"
+#include "uart.h"
 
 struct s_status
 {
@@ -46,7 +60,6 @@ uint8_t uart_error;
 #define UART_BAUD_RATE 57600
 
 //  Integer (Basis 10) rechtsbündig auf LCD ausgeben.
-
 void lcd_put_int(int16_t val, uint8_t len)
 {
 	char buffer[len+1];
@@ -67,18 +80,55 @@ void lcd_put_int32(int32_t val, uint8_t len)
 	lcd_puts(buffer);
 }
 
+void update_lcd()
+{
+  static float soll=98.7234;
+  static float ist=76.54321;
+  char buf[20];
+
+  //Solltemperatur
+  lcd_gotoxy(0,0);
+  _delay_ms(2);   //sonst zicks gotoxy rum, TODO: nachprüfen, ggf. Zeit verkleinern
+  lcd_puts_P("Soll:");
+  dtostrf(soll,4,1,buf);
+  lcd_puts(buf);
+  lcd_putc(0xDF);
+  lcd_putc('C');
+
+  //Status
+  lcd_putc(' ');
+  status.enable=0xFF;
+  lcd_putc((status.enable & _BV(0))? 'R':' ');
+  lcd_putc((status.enable & _BV(1))? 'H':' ');
+  lcd_putc((status.enable & _BV(2))? 'M':' ');
+  lcd_putc((status.enable & _BV(3))? 'D':' ');
+
+  //Isttemperatur
+  //lcd_gotoxy(0,1);
+  lcd_puts_P("Ist :");
+  dtostrf(ist,4,1,buf);
+  lcd_puts(buf);
+  lcd_putc(0xDF);
+  lcd_putc('C');
+
+  
+  //  	ltoa(ADC,buf,10);
+  //  	lcd_puts(buf);
+  //  	lcd_puts_P("    ");
+  soll+=0.1;
+  ist+=0.1;
+  if(soll>100.0) soll=50;
+  if(ist>100.0) ist=50;
+  
+}
+
+
 ISR(TIMER0_COMP_vect) //1kHz
 {
-    static uint8_t disp_cnt=0;
+  static uint8_t disp_cnt=0;
     
-    if (!disp_cnt++)
-    {
-    	lcd_gotoxy(0,1);
-    	char buf[20];
-    	ltoa(ADC,buf,10);
-    	lcd_puts(buf);
-    	lcd_puts_P("    ");
-    }
+  if (!disp_cnt++)
+    update_lcd();
 }
 
 int8_t sin_list[]={0,24,48,70,89,105,117,124,127,124,117,105,89,70,48,24,0,-24,-48,-70,-89,-105,-117,-124,-127,-124,-117,-105,-89,-70,-48,-24};
@@ -106,7 +156,7 @@ ISR(ADC_vect)
 // und eine Antwort mit allen Istwerten bzw. Status
 void processUART(void)
 {
-	//Alle Daten empfangen	
+	//Alle Daten empfangen
 	//momentan keine Fehlerbehandlung
 	if(uart_GetRXCount()>=sizeof(struct s_setvalues))
 	{
@@ -130,14 +180,23 @@ void processUART(void)
 
 int main(void)
 {
-	uart_init(UART_BAUD_SELECT_DOUBLE_SPEED(UART_BAUD_RATE,F_CPU));
+  uart_init(UART_BAUD_SELECT_DOUBLE_SPEED(UART_BAUD_RATE,F_CPU));
+  lcd_init(LCD_DISP_ON);
+  lcd_clrscr();
+  lcd_puts_P("Brautomat v0.1\n");
+  lcd_gotoxy(0,1);
+  lcd_puts_P(__DATE__" aw");
 
-    lcd_init(LCD_DISP_ON);
-    lcd_clrscr();
-    lcd_puts_P("Brautomat 0.1\n");
-    
-    DDRD |= _BV(PD4) | _BV(PD5) | _BV(PD6) | _BV(PD7);
-    PORTD |= _BV(PD6) | _BV(PD7);
+  //3s Delay for Splash
+  for(uint8_t i=0;i<2;++i)
+    _delay_ms(15);
+  lcd_clrscr();
+
+  //H-Brücke
+  DDRD |= _BV(PD4) | _BV(PD5) | _BV(PD6) | _BV(PD7);
+  //Relais für Heizung
+  DDRB |= _BV(PB3);
+  PORTD |= _BV(PD6) | _BV(PD7);
     
 	/*** TIMER0 ***/
 	OCR0=250;
@@ -160,7 +219,8 @@ int main(void)
 	//Prescaler 64 = 250Khz ADC Clock, AutoTrigger, Interrupts enable
 	ADCSRA = _BV(ADEN) | _BV(ADPS1) | _BV(ADPS2) | _BV(ADATE) | _BV(ADSC) | _BV(ADIE); 
 	//Interne 2.56V Reference verwenden
-	//Multiplexer auf Kanal2 (Poti)
+	//Multiplexer vorerst auf Kanal2 (Poti),
+  //TODO: später auf PA7, PT100
 	ADMUX = (_BV(REFS0) | _BV(REFS1)) + 2; 	
 	//ADC in Free Running mode
 	SFIOR &= ~(_BV(ADTS2) | _BV(ADTS1) | _BV(ADTS0));	
