af1ee3434af75f845b4e439bce04ca95b6d3c7ad
Andreas Weber <andy.weber.aw@gmail.com>
2013-04-06 18:59:00 +0200

    Dokumentation erweitert, wget für externe Datasheets

---
+++ Dokumentation/wget_datasheets.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -nc http://datasheets.maximintegrated.com/en/ds/DS18B20.pdf
+wget -nc www.atmel.com/Images/doc2503.pdf
+wget -nc http://download.savannah.gnu.org/releases/avr-libc/avr-libc-user-manual-1.8.0.pdf.bz2
