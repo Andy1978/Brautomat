0b15564ba70a83d4e51b1f1a04dbd715d669839a
Andreas Weber <andreas.weber@hs-offenburg.de>
2013-11-06 09:25:22 +0100

    Bilder vom ersten Versuch mit 12l wasser am 5.11.2013 hinzugefügt

--- GUI/show_log.m
+++ GUI/show_log.m
@@ -3,8 +3,11 @@
 ## second, usec since epoch, Isttemperatur, Solltemperatur, aktiver Schritt,
 ## Zeit verbleibend, Status Heizung
 
-d = load("cBrautomat_status.log");
+#d = load("cBrautomat_status.log");
 #d = load("efbe_schott_wasserkocher.log");
+
+## 12l Wasser, erster Test am 5.11.2013
+d = load("Wasser12l.log");
 
 t = d(:,1)+d(:,2)/1e6;
 t -= t(1);
@@ -14,7 +17,11 @@ active_step = d(:,5);
 time_remaining = d(:,6);
 state_heater = d(:,7);
 
-plot(t, T_ist, ";Isttemperatur;", t, T_soll, ";Solltemperatur;", t, state_heater)
+plot(t, T_ist, ";Ist;", t, T_soll, ";Soll;", t, state_heater*10,";Heizung;")
 grid on
 xlabel("Zeit [s]");
 ylabel("Temperatur [C]");
+legend("location","east")
+
+title("Test Nr.1 mit 12l Wasser")
+print("test1_12l_wasser.png")
